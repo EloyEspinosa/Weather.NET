7ea75b50a0b51a8f23e953193fe28920bcfa6a43
Eloy Espinosa <eloyespinosareinberg@gmail.com>
2021-11-29 16:50:58 -0300

    Deleted InvalidApiKeyException and finished GetCityJSON.

--- Current.cs
+++ Current.cs
@@ -11,6 +11,16 @@ namespace Weather.NET
     public static class Current
     {
         private static WebClient client = new WebClient();
+
+        /// <summary>
+        /// Gets the JSON file of the given search.
+        /// </summary>
+        /// <param name="name"> The name of the city. </param>
+        /// <param name="apiKey"> The api key of the user. </param>
+        /// <param name="measurement"> The type of measurement of the output. Can be: standard, metric or imperial. </param>
+        /// <param name="language"> The language of the output. Can be any of the given list: https://openweathermap.org/current#multi </param>
+        /// <returns> The JSON file as a string. </returns>
+        /// <exception cref="WebException"></exception>
         public static string GetCityJSON(string name, string apiKey, string measurement="standard", string language="en")
         {
             try
