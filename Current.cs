58c7284c87a55a7353f1d943c433339f9a476e4f
Eloy Espinosa <eloyespinosareinberg@gmail.com>
2021-11-29 16:04:31 -0300

    Initial commit.

---
+++ Current.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Weather.NET
+{
+    /// <summary>
+    /// Provides the current weather of a specific location.
+    /// OpenWeather docs: https://openweathermap.org/current
+    /// </summary>
+    public class Current
+    {
+        
+    }
+}
