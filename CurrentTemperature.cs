91c13a20c1041461700fab98515379291ba0b379
Eloy Espinosa <eloyespinosareinberg@gmail.com>
2021-11-29 21:29:56 -0300

    Added CurrentTemperature.GetWithCityName

--- CurrentTemperature.cs
+++ CurrentTemperature.cs
@@ -8,9 +8,17 @@ namespace Weather.NET
     /// </summary>
     public static class CurrentTemperature
     {
-        public static double GetWithCityName(string name, string apiKey, string format = "json", string measurement = "standard", string language = "en")
+        /// <summary>
+        /// Gets the current temperature in a given city.
+        /// More information in https://openweathermap.org/current#name
+        /// </summary>
+        /// <param name="name"> The name of the city. </param>
+        /// <param name="apiKey"> The api key of the user. </param>
+        /// <param name="measurement"> The type of measurement of the output. Can be: standard, metric or imperial. </param>
+        /// <returns> The current temperature as a double. </returns>
+        public static double GetWithCityName(string name, string apiKey, string measurement = "standard")
         {
-            string file = CurrentWeather.GetWithCityName(name, apiKey, format, measurement, language);
+            string file = CurrentWeather.GetWithCityName(name, apiKey, "json", measurement, "en");
             var json = JsonConvert.DeserializeObject<dynamic>(file);
             return json.main.temp;
         }
